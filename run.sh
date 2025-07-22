fb86f848373db4e6664661ed0847564478e02daa
IParsons1000 <163059817+IParsons1000@users.noreply.github.com>
2025-07-22 10:53:51 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -14,6 +14,6 @@ am start --user 0 -n com.termux.x11/com.termux.x11.MainActivity > /dev/null 2>&1
 
 sleep 1
 
-proot-distro login debian --shared-tmp -- /bin/bash -c 'export PULSE_SERVER=127.0.0.1 && export XDG_RUNTIME_DIR=${TMPDIR} && su - asq -c "env DISPLAY=:0 /usr/bin/Xwayland"'
+proot-distro login debian --shared-tmp -- /bin/bash -c 'export PULSE_SERVER=127.0.0.1 && export XDG_RUNTIME_DIR=${TMPDIR} && su - asq -c "env DISPLAY=:0 /usr/bin/weston --backend=x11-backend.so --xwayland"'
 
 exit 0
