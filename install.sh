0c8f29e2e58f998e179b6133bc9a6e3b4ca1ae73
IParsons1000 <163059817+IParsons1000@users.noreply.github.com>
2025-07-22 11:29:41 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,9 +7,9 @@ pkg install -y termux-x11-nightly pulseaudio wget
 
 proot-distro install debian
 wget https://github.com/cu233/proot_proc/raw/refs/heads/master/proc.tar.xz
-rm -rf filesystems || true
-mkdir filesystems
-tar xf proc.tar.xz -C filesystems
+rm -rf ~/filesystems || true
+mkdir ~/filesystems
+tar xf proc.tar.xz -C ~/filesystems
 cat <<EOF | proot-distro login debian --bind ~/filesystems:/proc/filesystems -- /usr/bin/bash
 apt update -y
 apt install sudo adduser -y
