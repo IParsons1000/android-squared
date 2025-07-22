5cab7445dfa603a5c84217734e5f923ed409051b
IParsons1000 <163059817+IParsons1000@users.noreply.github.com>
2025-07-22 11:34:25 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -14,6 +14,6 @@ am start --user 0 -n com.termux.x11/com.termux.x11.MainActivity > /dev/null 2>&1
 
 sleep 1
 
-proot-distro login debian --shared-tmp --bind ~/filesystems:/proc/filesystems -- /bin/bash -c 'export PULSE_SERVER=127.0.0.1 && export XDG_RUNTIME_DIR=~/tmp && su - asq -c "env DISPLAY=:0 XDG_RUNTIME_DIR=~/tmp /usr/bin/weston --backend=x11-backend.so --xwayland"'
+proot-distro login debian --shared-tmp --bind ~/filesystems:/proc/filesystems -- /bin/bash -c 'export PULSE_SERVER=127.0.0.1 && export XDG_RUNTIME_DIR=~/tmp && su - asq -c "env DISPLAY=:0 XDG_RUNTIME_DIR=/tmp /usr/bin/weston --backend=x11-backend.so --xwayland"'
 
 exit 0
