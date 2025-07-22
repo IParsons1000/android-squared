eb373d3b46f3fc043a6445a4f19fc5a3046331a2
IParsons1000 <163059817+IParsons1000@users.noreply.github.com>
2025-07-22 09:54:39 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,17 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-git clone https://github.com/antonyz89/docker-qemu-arm
-cd docker-qemu-arm
-./termux-setup.sh
-pkg install x11-repo
-pkg install termux-x11-nightly
-cd alpine
-sed -i -e 's/-nographic//g' startqemu.sh
-./startqemu.sh
+pkg update -y
+pkg install -y proot proot-distro
+pkg install -y x11-repo
+pkg install -y termux-x11-nightly
+
+proot-distro install debian
+cat <<EOF | proot-distro login debian -- /usr/bin/bash
+apt update -y
+apt install sudo adduser
+adduser asq
+usermod -a -G sudo asq
+echo 'asq    ALL=(ALL:ALL) ALL' >> /etc/sudoers
+apt install -y xorg xinit xterm
+exit
+EOF
